bb3574ac5d3fcbfe478294d4a4fa9f477ceeba51
linux_china <linux_china@hotmail.com>
2016-05-09 10:44:36 +0800

    thymeleaf 3 auto configuration

---
+++ src/main/java/org/springframework/boot/autoconfigure/thymeleaf3/Thymeleaf3AutoConfiguration.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.boot.autoconfigure.thymeleaf3;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.template.TemplateLocation;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.util.MimeType;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.Servlet;
+import java.util.LinkedHashMap;
+
+/**
+ * {@link EnableAutoConfiguration Auto-configuration} for Thymeleaf3.
+ *
+ * @author Dave Syer
+ * @author Andy Wilkinson
+ * @author Stephane Nicoll
+ * @author Brian Clozel
+ * @author Eddú Meléndez
+ */
+@Configuration
+@EnableConfigurationProperties({ThymeleafProperties.class})
+@ConditionalOnClass(SpringTemplateEngine.class)
+@AutoConfigureAfter(WebMvcAutoConfiguration.class)
+@AutoConfigureBefore(ThymeleafAutoConfiguration.class)
+public class Thymeleaf3AutoConfiguration {
+    private static final Log logger = LogFactory.getLog(Thymeleaf3AutoConfiguration.class);
+
+    @Configuration
+    public static class DefaultTemplateResolverConfiguration implements ApplicationContextAware {
+        @Autowired
+        private ThymeleafProperties properties;
+
+        private ApplicationContext applicationContext;
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            this.applicationContext = applicationContext;
+        }
+
+        @PostConstruct
+        public void checkTemplateLocationExists() {
+            boolean checkTemplateLocation = this.properties.isCheckTemplateLocation();
+            if (checkTemplateLocation) {
+                TemplateLocation location = new TemplateLocation(
+                        this.properties.getPrefix());
+                if (!location.exists(this.applicationContext)) {
+                    logger.warn("Cannot find template location: " + location
+                            + " (please add some templates or check "
+                            + "your Thymeleaf configuration)");
+                }
+            }
+        }
+
+        @Bean
+        //made this @Bean (vs private in Thymeleaf migration docs ), otherwise MessageSource wasn't autowired.
+        public TemplateEngine templateEngine() {
+            SpringTemplateEngine engine = new SpringTemplateEngine();
+            engine.setTemplateResolver(templateResolver());
+            return engine;
+        }
+
+        private ITemplateResolver templateResolver() {
+            SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+            resolver.setApplicationContext(applicationContext);
+            resolver.setPrefix(this.properties.getPrefix());
+            resolver.setSuffix(this.properties.getSuffix());
+            resolver.setTemplateMode(this.properties.getMode());
+            resolver.setCacheable(this.properties.isCache());
+            return resolver;
+        }
+
+        @Bean
+        public ViewResolver viewResolver() {
+            ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+            resolver.setOrder(2147483642);
+            resolver.setTemplateEngine(templateEngine());
+            resolver.setCharacterEncoding("UTF-8");
+            return resolver;
+        }
+
+        @Bean
+        public ITemplateResolver defaultTemplateResolver() {
+            SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+            resolver.setApplicationContext(applicationContext);
+            resolver.setPrefix(this.properties.getPrefix());
+            resolver.setSuffix(this.properties.getSuffix());
+            resolver.setTemplateMode(this.properties.getMode());
+            if (this.properties.getEncoding() != null) {
+                resolver.setCharacterEncoding(this.properties.getEncoding().name());
+            }
+            resolver.setCacheable(this.properties.isCache());
+            Integer order = this.properties.getTemplateResolverOrder();
+            if (order != null) {
+                resolver.setOrder(order);
+            }
+            return resolver;
+        }
+    }
+
+    @Configuration
+    @ConditionalOnClass({Servlet.class})
+    @ConditionalOnWebApplication
+    protected static class ThymeleafViewResolverConfiguration {
+        @Autowired
+        private ThymeleafProperties properties;
+        @Autowired
+        private SpringTemplateEngine templateEngine;
+
+        @Bean
+        @ConditionalOnMissingBean(name = "thymeleafViewResolver")
+        @ConditionalOnProperty(name = "spring.thymeleaf.enabled", matchIfMissing = true)
+        public ThymeleafViewResolver thymeleafViewResolver() {
+            ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+            resolver.setTemplateEngine(this.templateEngine);
+            resolver.setCharacterEncoding(this.properties.getEncoding().name());
+            resolver.setContentType(appendCharset(this.properties.getContentType(),
+                    resolver.getCharacterEncoding()));
+            resolver.setExcludedViewNames(this.properties.getExcludedViewNames());
+            resolver.setViewNames(this.properties.getViewNames());
+            // This resolver acts as a fallback resolver (e.g. like a
+            // InternalResourceViewResolver) so it needs to have low precedence
+            resolver.setOrder(Ordered.LOWEST_PRECEDENCE - 5);
+            resolver.setCache(this.properties.isCache());
+            return resolver;
+        }
+
+        private String appendCharset(MimeType type, String charset) {
+            if (type.getCharSet() != null) {
+                return type.toString();
+            }
+            LinkedHashMap<String, String> parameters = new LinkedHashMap<String, String>();
+            parameters.put("charset", charset);
+            parameters.putAll(type.getParameters());
+            return new MimeType(type, parameters).toString();
+        }
+
+    }
+
+
+}
